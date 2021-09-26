ba432eeae7331dd0306c6b6c8011ab3b9ea91a1f
emuroku <m610310@gmail.com>
2021-09-27 00:24:48 +0900

    リクエスト・レスポンス練習 #1

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,59 +11,11 @@ use Validator;
 class HelloController extends Controller
 {
     public function index(Request $request){
-
-        $validator = Validator::make($request->query(),[
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-
-        if($validator->fails()){
-            $msg = 'クエリに問題があります';
-        }else{
-            $msg = 'ID/PASSを受け付けました。フォームを入力してください';
-        }
-
-        return view('hello.index', ['msg'=>$msg]);
+        return view('hello.index', ['msg'=>'フォームを入力してください']);
     }
 
-    public function post(Request $request){
-        
-       // バリデーションのルール配列を設定
-       $rules = [
-           'name' => 'required',
-           'mail' => 'email',
-           'age' => 'numeric',
-       ];
-
-       // エラーメッセージ配列を設定
-       $messages = [
-           'name.required' => '名前は必ず入力して下さい',
-           'mail.email' => 'メールアドレスが必要です',
-           'age.numeric' =>'年齢を整数で記入して下さい',
-           'age.min' => '年齢はゼロ歳以上で記入して下さい',
-           'age.max' => '年齢は200歳以下で記入して下さい',
-       ];
-
-       // バリデータの呼び出し
-       $validator = Validator::make($request->all(), $rules, $messages);
-
-       // sometimesメソッドによるルールの追加
-       $validator -> sometimes('age', 'min:0', function($input){
-           return is_numeric($input->age);
-       });
-
-       $validator -> sometimes('age', 'max:200', function($input){
-           return is_numeric($input->age);
-       });
-
-
-       if($validator->fails()){
-           return redirect('/hello')
-                -> withErrors($validator)
-                -> withInput();
-       }
-
-       return view('hello.index', ['msg' => '正しく入力されました！']);
+    public function post(HelloRequest $request){
+        return view('hello.index', ['msg' => '正しく入力されました']);
     }
 
 
